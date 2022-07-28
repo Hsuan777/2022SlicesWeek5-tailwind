df795f72b3d9e2f22cb862a9242ecc7b02329121
Vic <syuan0212@gmail.com>
2022-07-28 16:46:14 +0800

    add: header navbar bg-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
     extend: {},
+    colors: {
+      "primary": {
+        DEFAULT: '#AA0601',
+        dark: '#650300'
+      },
+      "secondary":'#FBF2F2',
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
