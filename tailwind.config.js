7cd76f6ae8dff78ea739b6fb5d859982c12612bd
Vic <syuan0212@gmail.com>
2022-07-31 11:53:01 +0800

    add: blogList html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,28 @@ module.exports = {
     extend: {},
     colors: {
       "primary": {
-        DEFAULT: '#AA0601',
-        dark: '#650300'
+        DEFAULT: "#AA0601",
+        dark: "#650300"
       },
-      "secondary":'#FBF2F2',
-      "white": '#ffffff',
-      "black": '#000000'
+      "secondary": "#FBF2F2",
+      "white": "#ffffff",
+      "black": "#000000"
+    },
+    fontSize: {
+      "sm": ["14px", "1.5"],
+      "base":["16px", "1.5"],
+      "lg": ["18px", "1.5"],
+      "xl": ['20px', "1.5"],
+      "2xl": ["24px", "1.5"],
+      "3xl": ["32px", "1.5"],
+      "4xl": ["36px", "1.5"],
+      "5xl": ["42px", "1.5"],
+      "6xl": ["48px", "1.5"],
     }
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
   ],
 }
