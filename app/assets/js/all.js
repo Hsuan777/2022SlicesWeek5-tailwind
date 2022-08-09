9ed07fe9eb2f8fcd190fee90f59f8a8f5f1a3c3f
Vic <syuan0212@gmail.com>
2022-08-10 00:35:42 +0800

    add: reply effect

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,9 @@
 console.log('Hello!');
 
 $(function() {
-
+  $('#reply').on('click', () => {
+    $('#replyTextarea').toggleClass('hidden');
+    $('#replyBtnGroup').toggleClass('-bottom-[3.5rem] bottom-5 right-5');
+    $('#replied').toggleClass('hidden');
+  })
 })
