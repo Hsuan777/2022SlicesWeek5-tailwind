e21705f54092eee5df287a4e7c8f355d4d680e4f
Vic <syuan0212@gmail.com>
2022-08-10 21:44:03 +0800

    Update 2022-08-10T13:43:36.069Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,11 @@
-console.log('Hello!');
+"use strict";
 
-$(function() {
-  $('#reply').on('click', () => {
+console.log('Hello!');
+$(function () {
+  $('#reply').on('click', function () {
     $('#replyTextarea').toggleClass('hidden');
     $('#replyBtnGroup').toggleClass('-bottom-[3.5rem] bottom-5 right-5');
     $('#replied').toggleClass('hidden');
-  })
-})
+  });
+});
+//# sourceMappingURL=all.js.map
